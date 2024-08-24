ae3c854f0f219c36c924f8cb72a67b198f586ffa
jhwlim <jhwlim25@gmail.com>
2024-08-24 17:05:48 +0900

    Initial commit

---
+++ src/main/kotlin/io/webapp/moa/MoaApplication.kt
@@ -0,0 +1,11 @@
+package io.webapp.moa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoaApplication
+
+fun main(args: Array<String>) {
+    runApplication<MoaApplication>(*args)
+}
---
+++ src/test/kotlin/io/webapp/moa/MoaApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.webapp.moa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
