9b693b83abf32b547f0a11b503ed6ea90bb0e584
jhwlim <jhwlim25@gmail.com>
2024-08-25 23:24:30 +0900

    test: Add tests for save() in UserRepository

--- src/test/kotlin/io/webapp/moa/user/domain/repository/UserRepositoryTest.kt
+++ src/test/kotlin/io/webapp/moa/user/domain/repository/UserRepositoryTest.kt
@@ -1,6 +1,7 @@
 package io.webapp.moa.user.domain.repository
 
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.webapp.moa.SpringTestSpec
 import io.webapp.moa.user.domain.model.aggregate.User
 import io.webapp.moa.user.domain.model.value.Email
@@ -13,6 +14,26 @@ class UserRepositoryTest(
     private val userRepository: UserRepository,
     private val testEntityManager: TestEntityManager,
 ) : SpringTestSpec({
+
+    describe("save") {
+
+        context("새로운 사용자가 등록되었을 때") {
+
+            val user = User(
+                email = Email("test@example.com"),
+                password = EncryptedPassword("password"),
+                name = "Test User"
+            )
+
+            val actual = userRepository.save(user)
+
+            it("id는 0이 아니어야 한다.") {
+                actual.id shouldNotBe 0L
+            }
+
+        }
+
+    }
 
     describe("findByEmail") {
 
@@ -31,8 +52,9 @@ class UserRepositoryTest(
 
             val email = Email("test@example.com")
 
+            val actual = userRepository.findByEmail(email)
+
             it("해당 이메일을 가진 사용자를 반환해야 한다.") {
-                val actual = userRepository.findByEmail(email)
                 actual shouldBe user
             }
 
@@ -42,8 +64,9 @@ class UserRepositoryTest(
 
             val email = Email("test22@example.com")
 
+            val actual = userRepository.findByEmail(email)
+
             it("null 을 반환해야 한다.") {
-                val actual = userRepository.findByEmail(email)
                 actual shouldBe null
             }
 
